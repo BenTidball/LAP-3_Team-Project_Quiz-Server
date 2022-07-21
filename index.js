6c39eb6e19b7629d6ae16001b146d90f595bfca8
Ben <bentidball97@gmail.com>
2022-07-21 08:46:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,22 +14,50 @@ const io = new Server(server, {
     },
 });
 
+
 io.on("connection", (socket) => {
     console.log('user connected: ', socket.id);
-
-    socket.on('join_room', (data) => {
-        socket.join(data);
-        console.log('room joined: ', data)
-    })
-
+   
+    //debugging purposes
     socket.on('send_message', (data) => {
         console.log("message sent from: ", data.room)
         socket.to(data.room).emit('recieve_message', data.message);
     })
 
+    let hostName;
+
+    //creates a room if player is host
+    socket.on('create_room', (data) => {
+        hostName = data.username;
+        socket.join(data.room);
+        console.log('room created: ', data.room);
+    })
+
+    //joins a room if player is not host
+    socket.on('join_room', (data) => {
+        socket.join(data.room);
+        socket.to(data.room).emit('recieve_host_name', hostName);
+        socket.to(data.room).emit('recieve_player_data', data)
+        console.log('room joined: ', data.room)
+    })
+   
+    //start game of all players in room
     socket.on('host_start_game', (data) => {
         console.log("Host has started game: ", data.roomNum)
-        socket.to(data.roomNum).emit('recieve_host_start', {hostStart: true});
+        socket.to(data.roomNum).emit('recieve_host_start', {hostStart: true, questions: data.questions});
+    })
+   
+    //send player list to room
+    socket.on('update_player_list', (data) => {
+        socket.to(data.room).emit('recieve_updated_player_list', {players: data.players});
+        console.log(data.players);
+    })
+
+    //emit player choices to room
+    socket.on('player_choice', (data) => {
+        socket.to(data.room).emit('recieve_player_choices', 
+        {user: data.username, id: data.socket.id, choice: data.choiceID, room: data.roomNum});
+        console.log(`user ${data.username} chose answer ${data.choiceID}`);
     })
 
 })
